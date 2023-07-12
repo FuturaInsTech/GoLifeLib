7c3a2115f80ae5f835dea5a9591418473e3acf5d
Ranga <rangarajan.ramanujam@gmail.com>
2023-07-12 09:51:49 +0530

    maturity model again

--- models/maturityh.go
+++ models/maturityh.go
@@ -8,22 +8,21 @@ import (
 type MaturityH struct {
 	gorm.Model
 	types.CModel
-	PolicyID          uint
-	ClientID          uint
-	EffectiveDate     string `gorm:"type:varchar(08)"`
-	MaturityDate      string `gorm:"type:varchar(08)"`
-	Status            string `gorm:"type:varchar(02)"`
-	BillDate          string `gorm:"type:varchar(08)"`
-	PaidToDate        string `gorm:"type:varchar(08)"`
-	Product           string `gorm:"type:varchar(05)"`
-	AplAmount         float64
-	LoanAmount        float64
-	PolicyDepost      float64
-	CashDeposit       float64
-	RefundPrem        float64
-	PremTolerance     float64
-	TotalSurrPayable  float64
-	AdjustedAmount    float64
-	ReasonDescription string `gorm:"type:varchar(1000)"`
-	MaturityDs        []MaturityD
+	PolicyID             uint
+	ClientID             uint
+	EffectiveDate        string `gorm:"type:varchar(08)"`
+	MaturityDate         string `gorm:"type:varchar(08)"`
+	Status               string `gorm:"type:varchar(02)"`
+	BillDate             string `gorm:"type:varchar(08)"`
+	PaidToDate           string `gorm:"type:varchar(08)"`
+	Product              string `gorm:"type:varchar(05)"`
+	AplAmount            float64
+	LoanAmount           float64
+	PolicyDepost         float64
+	CashDeposit          float64
+	RefundPrem           float64
+	PremTolerance        float64
+	TotalMaturityPayable float64
+	AdjustedAmount       float64
+	MaturityDs           []MaturityD
 }

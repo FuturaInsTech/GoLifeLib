245e0103077cedddce100cc3a620470c8ed1b54c
Ranga <rangarajan.ramanujam@gmail.com>
2023-09-02 12:16:48 +0530

    changes in ilp

--- models/ILP.go
+++ models/ILP.go
@@ -16,6 +16,7 @@ type IlpPrice struct {
 	FundBidPrice   float64
 	FundOfferPrice float64
 	FundSeqno      uint
+	ApprovalFlag   string `gorm:"type:varchar(2)"`
 }
 
 type IlpSummary struct {
@@ -31,18 +32,25 @@ type IlpSummary struct {
 type IlpTransaction struct {
 	gorm.Model
 	types.CModel
-	PolicyID         uint
-	BenefitID        uint
-	FundCode         string `gorm:"type:varchar(5)"` //P0050
-	FundType         string `gorm:"type:varchar(2)"` //ACcummulated/INinitial/BOus P0050
-	TransactionDate  string `gorm:"type:varchar(8)"`
-	FundEffDate      string `gorm:"type:varchar(8)"`
-	FundAmount       float64
-	FundCurrency     string `gorm:"type:varchar(3)"`
-	FundUnits        float64
-	FundPrice        float64
-	CurrentOrFuture  string `gorm:"type:varchar(1)"` //P0050
-	OriginalAmount   float64
-	OriginalCurrency string `gorm:"type:varchar(3)"`
-	HistoryCode      string `gorm:"type:varchar(5)"`
+	PolicyID            uint
+	BenefitID           uint
+	FundCode            string `gorm:"type:varchar(5)"` //P0050
+	FundType            string `gorm:"type:varchar(2)"` //ACcummulated/INinitial/BOus P0050
+	TransactionDate     string `gorm:"type:varchar(8)"`
+	FundEffDate         string `gorm:"type:varchar(8)"`
+	FundAmount          float64
+	FundCurrency        string `gorm:"type:varchar(3)"`
+	FundUnits           float64
+	FundPrice           float64
+	CurrentOrFuture     string `gorm:"type:varchar(1)"` //P0050
+	OriginalAmount      float64
+	OriginalCurrency    string `gorm:"type:varchar(3)"`
+	HistoryCode         string `gorm:"type:varchar(5)"`
+	InvNonInvFlag       string `gorm:"type:varchar(2)"`
+	InvNonInvPercentage float64
+	AccountCodeID       uint
+	AccountCode         string `gorm:"type:varchar(30)"`
+	CurrencyRate        float64
+	MortalityIndicator  string `gorm:"type:varchar(1)"`
+	SurrenderPercentage float64
 }

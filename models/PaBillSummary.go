9375129d121969657519da4654f364e53f2c64a5
sreeramr22 <sreerar22@gmail.com>
2024-01-03 10:35:01 +0530

    PaBillSummary Model Creation

---
+++ models/PaBillSummary.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/FuturaInsTech/GoLifeLib/types"
+	"gorm.io/gorm"
+)
+
+type PaBillSummary struct {
+	gorm.Model
+	types.CModel
+	PaID               uint
+	PaBillDueMonth     string `gorm:"type:varchar(06)"` // YYYYMM
+	PaBillSeqNo        uint
+	PaBillStatus       string `gorm:"type:varchar(01)"` // P0050
+	ExtractedDate      string `gorm:"type:varchar(8)"`
+	ExtractedCount     uint
+	ExtractedAmount    float64
+	DeductedCount      uint
+	DeductedAmount     float64
+	NotDeductedCount   uint
+	NotDeductedAmount  float64
+	UnReconciledCount  uint
+	UnReconciledAmount float64
+	ReconciledDate     string `gorm:"type:varchar(8)"`
+	ReconciledBy       string `gorm:"type:varchar(30)"`
+	ApprovedDate       string `gorm:"type:varchar(8)"`
+	ApprovedBy         string `gorm:"type:varchar(30)"`
+}

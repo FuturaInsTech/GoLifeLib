0878a6bc035f465133040f84acb3074512f5125d
sreeramr22 <sreerar22@gmail.com>
2024-01-16 17:51:44 +0530

    PaBillSummary model moved into PayingAuthority.go & Interface variable functions

--- models/PayAuthority.go
+++ models/PayAuthority.go
@@ -21,3 +21,27 @@ type PayingAuthority struct {
 	PaCurrency      string `gorm:"type:varchar(03)"`
 	PaBillSummaries []PaBillSummary
 }
+
+type PaBillSummary struct {
+	gorm.Model
+	types.CModel
+	PayingAuthorityID  uint
+	PaName             string `gorm:"type:varchar(50)"`
+	PaType             string `gorm:"type:varchar(01)"`
+	PaBillDueMonth     string `gorm:"type:varchar(06)"` // YYYYMM
+	PaBillSeqNo        uint
+	PaBillStatus       string `gorm:"type:varchar(01)"` // P0050
+	ExtractedDate      string `gorm:"type:varchar(8)"`
+	ExtractedCount     uint
+	ExtractedAmount    float64
+	DeductedCount      uint
+	DeductedAmount     float64
+	NotDeductedCount   uint
+	NotDeductedAmount  float64
+	UnReconciledCount  uint
+	UnReconciledAmount float64
+	ReconciledDate     string `gorm:"type:varchar(8)"`
+	ReconciledBy       string `gorm:"type:varchar(30)"`
+	ApprovedDate       string `gorm:"type:varchar(8)"`
+	ApprovedBy         string `gorm:"type:varchar(30)"`
+}

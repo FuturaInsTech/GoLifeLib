1a5200dc246957b454c2708954dc5dd954357fa7
sreeramr22 <sreerar22@gmail.com>
2024-12-23 10:43:56 +0530

    Update workflow.go

--- models/workflow.go
+++ models/workflow.go
@@ -15,3 +15,13 @@ type WorkflowComments struct {
 	WorkflowComment string `gorm:"type:varchar(2500)"`
 	CommentProof    string `gorm:"type:longtext"`
 }
+
+type WorkflowRules struct {
+	types.CModel
+	WorkflowSeqno        uint
+	WorkflowType         string `gorm:"type:varchar(20)"`
+	WorkflowDescription  string `gorm:"type:varchar(50)"`
+	WorkflowSubType      string `gorm:"type:varchar(20)"`
+	WorkflowSubTypeDesc  string `gorm:"type:varchar(50)"`
+	WorkflowOptIndicator string `gorm:"type:varchar(1)"` // M Mandatory O Option
+}

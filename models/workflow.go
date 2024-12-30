3d9a7fe7061817e6e3af3d94f8ecd5cf80de131d
shubhampatilfutura <shubham.patil@futurainstech.com>
2024-12-30 15:56:56 +0530

    included the new workflow tables in workflow modal

--- models/workflow.go
+++ models/workflow.go
@@ -1,20 +1,22 @@
 package models
 
 import (
+	"time"
+
 	"github.com/FuturaInsTech/GoLifeLib/types"
 	"gorm.io/gorm"
 )
 
-type WorkflowComments struct {
-	gorm.Model
-	types.CModel
-	PolicyID        uint
-	ClientID        uint
-	UserID          uint
-	Sequence        uint
-	WorkflowComment string `gorm:"type:varchar(2500)"`
-	CommentProof    string `gorm:"type:longtext"`
-}
+// type WorkflowComments struct {
+// 	gorm.Model
+// 	types.CModel
+// 	PolicyID        uint
+// 	ClientID        uint
+// 	UserID          uint
+// 	Sequence        uint
+// 	WorkflowComment string `gorm:"type:varchar(2500)"`
+// 	CommentProof    string `gorm:"type:longtext"`
+// }
 
 type WorkflowRules struct {
 	types.CModel
@@ -26,15 +28,70 @@ type WorkflowRules struct {
 	WorkflowOptIndicator string `gorm:"type:varchar(1)"` // M Mandatory O Option
 }
 
-type WorkflowPolicy struct {
+// type WorkflowPolicy struct {
+// 	gorm.Model
+// 	types.CModel
+// 	PolicyID             uint
+// 	WorkflowType         string `gorm:"type:varchar(20)"`
+// 	WorkflowTypeRef      string `gorm:"type:varchar(20)"`
+// 	WorkflowSeqno        uint
+// 	WorkflowDescription  string `gorm:"type:varchar(50)"`
+// 	WorkflowSubType      string `gorm:"type:varchar(20)"`
+// 	WorkflowSubTypeDesc  string `gorm:"type:varchar(50)"`
+// 	WorkflowOptIndicator string `gorm:"type:varchar(1)"` // M Mandatory O Option
+// }
+
+type WorkflowRequest struct {
 	gorm.Model
 	types.CModel
-	PolicyID             uint
-	WorkflowType         string `gorm:"type:varchar(20)"`
-	WorkflowTypeRef      string `gorm:"type:varchar(20)"`
-	WorkflowSeqno        uint
-	WorkflowDescription  string `gorm:"type:varchar(50)"`
-	WorkflowSubType      string `gorm:"type:varchar(20)"`
-	WorkflowSubTypeDesc  string `gorm:"type:varchar(50)"`
-	WorkflowOptIndicator string `gorm:"type:varchar(1)"` // M Mandatory O Option
+	ReqName        string `gorm:"type:varchar(20)"`
+	ReqDescription string `gorm:"type:longtext"`
+	CreatedBy      uint
+	ReqRefData     string `gorm:"type:varchar(20)"`
+	ReqRefType     string `gorm:"type:varchar(20)"`
+	ReqStatus      string `gorm:"type:varchar(20)"`
+	WfTasks        []WfTask
+}
+
+type WfTask struct {
+	gorm.Model
+	types.CModel
+	RequestID        uint
+	TaskName         string `gorm:"type:varchar(20)"`
+	TaskDescription  string `gorm:"type:longtext"`
+	SeqNo            uint
+	SlaDuration      uint
+	TaskStatus       string `gorm:"type:varchar(2)"`
+	Priority         string `gorm:"type:varchar(2)"`
+	DueDate          string `gorm:"type:varchar(8)"`
+	WfTaskAssignment WfTaskAssignment
+	ExecutionLogs    []WfTaskExecutionLog
+}
+
+type WfTaskAssignment struct {
+	gorm.Model
+	types.CModel
+	TaskID       uint
+	AssignedTo   uint
+	AssignedAt   time.Time
+	StartedAt    time.Time
+	CompletedAt  time.Time
+	SlaViolation bool   `json:"is_active"`
+	Comments     string `gorm:"type:longtext"`
+	Attachments  string `gorm:"type:longtext"`
+}
+
+type WfTaskExecutionLog struct {
+	gorm.Model
+	types.CModel
+	TaskID       uint
+	AssignedTo   uint
+	AssignedAt   time.Time
+	StartedAt    time.Time
+	CompletedAt  time.Time
+	SlaViolation bool `json:"is_active"`
+	LoggedAt     time.Time
+	// action      string `gorm:"type:varchar(10)"` //sub-tasks of tasks ??
+	// PerformedBy uint
+	Details string `gorm:"type:longtext"`
 }

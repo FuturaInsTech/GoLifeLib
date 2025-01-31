68ba4533e623a4a25c1c1dbcdaa5ee1cb79e930b
sreeramr22 <sreerar22@gmail.com>
2025-01-31 14:12:40 +0530

    Update syncDatabase.go

--- initializers/syncDatabase.go
+++ initializers/syncDatabase.go
@@ -19,7 +19,7 @@ func SyncDatabase() {
 	// DB.AutoMigrate(&models.User{})
 	// DB.AutoMigrate(&models.Coverage{})
 	// DB.AutoMigrate(&models.Load{})
-	// DB.AutoMigrate(&models.Client{})
+	DB.AutoMigrate(&models.Client{})
 	// DB.AutoMigrate(&models.ParamDesc{})
 	// DB.AutoMigrate(&models.Param{})
 	// DB.AutoMigrate(&models.Address{})

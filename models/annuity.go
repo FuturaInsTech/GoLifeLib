2cac6f1975e15336c40aa7aa8fa4b35b97345462
Ranga <rangarajan.ramanujam@gmail.com>
2025-02-04 16:49:38 +0530

    Annuity changes

--- models/annuity.go
+++ models/annuity.go
@@ -10,17 +10,17 @@ type Annuity struct {
 	types.CModel
 	PolicyID     uint
 	ClientID     uint
-	AnnAmount    float64
+	AnnAmount    float64 // Annuity Amount
 	AnnPecentage float64
 	BenefitID    uint
-	AnnType      string `gorm:"type:varchar(05)"` //01    /P0050
-	BCoverage    string `gorm:"type:varchar(05)"` //Q0006
-	AnnSA        float64
-	AnnEndDate   string `gorm:"type:varchar(08)"` //99999999
-	AnnStartDate string `gorm:"type:varchar(08)"` //20230101
-	AnnCurrDate  string `gorm:"type:varchar(08)"` //20250201
-	AnnNxtDate   string `gorm:"type:varchar(08)"` //20250301
-	AnnFreq      string `gorm:"type:varchar(02)"` //12
-	CntCurr      string `gorm:"type:varchar(03)"`
-	PayCurr      string `gorm:"type:varchar(03)"`
+	AnnType      string  `gorm:"type:varchar(05)"` //01    /P0050
+	BCoverage    string  `gorm:"type:varchar(05)"` //Q0006
+	AnnSA        float64 // Purpose Amount or Contribution
+	AnnEndDate   string  `gorm:"type:varchar(08)"` //99999999
+	AnnStartDate string  `gorm:"type:varchar(08)"` //20230101
+	AnnCurrDate  string  `gorm:"type:varchar(08)"` //20250201
+	AnnNxtDate   string  `gorm:"type:varchar(08)"` //20250301
+	AnnFreq      string  `gorm:"type:varchar(02)"` //12
+	CntCurr      string  `gorm:"type:varchar(03)"` //INR
+	PayCurr      string  `gorm:"type:varchar(03)"` //USD
 }

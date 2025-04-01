d33ad480870b3b20821b30f449a654818fcb7381
sreeramr22 <sreerar22@gmail.com>
2025-04-01 17:55:30 +0530

    Update planlife.go

--- models/planlife.go
+++ models/planlife.go
@@ -22,10 +22,19 @@ type PlanLife struct {
 	PSmoker        string `gorm:"type:varchar(1)"`
 	PStartDate     string `gorm:"type:varchar(08)"`
 	PSumAssured    uint64
-	PBasAnnualPrem float64    // Annualized Premium Before Applying Discount and Factor
-	PLoadPrem      float64    // Loaded Premium
-	PDiscPrem      float64    // Total Premium Discount
-	PPrem          float64    // Total Premium
-	PDiscountType  [5]string  `gorm:"type:varchar(01)"` // Discount Type
-	PDiscountPrem  [5]float64 // Premium Discount
+	PBasAnnualPrem float64 // Annualized Premium Before Applying Discount and Factor
+	PLoadPrem      float64 // Loaded Premium
+	PDiscPrem      float64 // Total Premium Discount
+	PPrem          float64 // Total Premium
+	PDisType01     string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisPrem01     float64 // Premium Discount
+	PDisType02     string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisPrem02     float64 // Premium Discount
+	PDisType03     string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisPrem03     float64 // Premium Discount
+	PDisType04     string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisPrem04     float64 // Premium DiscountPDiscountType  [5]string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisType05     string  `gorm:"type:varchar(01)"` // Discount Type
+	PDisPrem05     float64 // Premium DiscountPDiscountType  [5]string  `gorm:"type:varchar(01)"` // Discount Type
+
 }

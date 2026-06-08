02fda9ead13d092c7cde0a3e5442897ecb8b4847
sreeramr22 <sreerar22@gmail.com>
2026-06-08 15:18:08 +0530

    Update png2html.bat

--- cmds/png2html.bat
+++ cmds/png2html.bat
@@ -1,5 +1,13 @@
 @echo off
 
-set PATH=%~dp0tesslib;%PATH%
+REM Get the folder where the BAT file resides
+set "BASEDIR=%~dp0"
 
-"%~dp0pngtohtml.exe" %*
+REM Add Tesseract DLL folder to PATH
+set "PATH=%BASEDIR%tesslib;%PATH%"
+
+REM Tell Tesseract where trained data files are located
+set "TESSDATA_PREFIX=%BASEDIR%tessdata"
+
+REM Execute program and pass all arguments
+"%BASEDIR%pngtohtml.exe" %*
